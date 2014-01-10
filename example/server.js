8e356885eb3a1af44ea1a8e9403b5356eb509f53
James Halliday <mail@substack.net>
2014-01-09 17:12:04 -0800

    hypothetical target output

--- example/server.js
+++ example/server.js
@@ -7,9 +7,10 @@ var server = http.createServer(function (req, res) {
     var repo = req.url.split('/')[1];
     var dir = path.join(__dirname, 'repos', repo);
     
-    console.log(req.url);
     req.pipe(backend(req.url, function (err, service) {
         if (err) return res.end(err + '\n');
+        
+        console.log(service.action, service.repo, service.branch);
         
         var ps = spawn(service.name, service.args.concat(dir));
         ps.stdout.pipe(service.createStream()).pipe(ps.stdin);
